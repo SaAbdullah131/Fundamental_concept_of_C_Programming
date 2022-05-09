22d796a5faed3be22deb10edc5ac856ffca8c8f1
SaAbdullah131 <cse.asabdullah@gmail.com>
2022-05-10 02:11:04 +0600

    Control flow and conditional statement

---
+++ Control_flow_Conditional_statement/simple_if.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+int marks;
+scanf("%d",&marks);
+
+if(marks >=40){
+    printf("You have Passed \n");
+    printf("Congratulations!");
+}
+else {
+    printf("You have Failed.\n");
+}
+return 0;
+}
+/// if rain,umbrella carry on.
+/* if(conditions){
+        statements;
+}
+else{
+    statements;
+}
+*/
