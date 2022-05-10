3dd02f6b27ec4705aafd1d7dbb53c54087285709
SaAbdullah131 <cse.asabdullah@gmail.com>
2022-05-10 22:33:32 +0600

    Conditional Statement

---
+++ Control_flow_Conditional_statement/Relational_operator_variable.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+int a=8,b=10;
+int comp = (a<b); // return a boolean value;
+
+/* int comp =95;
+if(comp)// comp is true except 0{
+    printf("comp is true"); //
+}
+*/
+printf("comp = %d\n",comp);
+
+if(comp){
+    printf("comp is true.\n");
+}
+else{
+    printf("Comp is false.\n");
+}
+
+return 0;
+}
+
+/// >->greater than, < =less than,>= -> greater than or equal to ,<=less than or equal to,== equal to,=! -> not equal to
+/// =->Assignment
+/// ==-> equal to
+
---
+++ Control_flow_Conditional_statement/postive_negative.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num>0)
+    {
+        printf("Positive\n");
+    }
+    else
+    {
+        if(num==0)
+        {
+            printf("Zero\n");
+        }
+        else
+        {
+            printf("Negative\n");
+        }
+    }
+    return 0;
+}
+/// 2 ->positive
+/// 0 ->zero
+/// -5 ->Negative
